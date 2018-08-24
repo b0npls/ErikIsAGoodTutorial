07eb87a52b48765d60e8eee7e90a4504db0f44ec
SimonSchama <ejl3tf@virginia.edu>
2018-08-23 21:54:05 -0400

    wip wispy woods

---
+++ Team Spooky Tree/Assets/Scripts/Puff.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puff : MonoBehaviour {
+    int dirMult = 0;
+    public void SetDirection(bool dir)
+    {
+        dirMult = dir ? -1 : 1;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(this.transform.position.x + dirMult * 0.05f, this.transform.position.y);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+
+        }
+    }
+}
---
+++ Team Spooky Tree/Assets/Scripts/WispyWoods.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WispyWoods : MonoBehaviour {
+    Rigidbody2D rb;
+    public Puff p;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity.Set(-1, 5);
+	}
+	public void shootPuff()
+    {
+        Puff newP = Instantiate(p, new Vector3(transform.position.x - 0.3f, transform.position.y - 0.35f), transform.rotation);
+        newP.SetDirection(true);
+    }
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Animator anim = GetComponent<Animator>();
+            anim.SetTrigger("puffTrigger");
+        }
+
+	}
+}
