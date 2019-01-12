e662d2439bf3b050886d3247b22783cf28b2892c
b0npls <bam008@eagles.bridgewater.edu>
2019-01-12 13:11:40 -0500

    WoodmanExe with 4 moves added; 2 new character select icons added; woomandexe shield does not block attacks unforetunately; this is what folks should use and work from

---
+++ Team Spooky Tree/Assets/Scripts/BonHealing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonHealing : MonoBehaviour {
+
+	public float healing = 2f;
+	public float upForce, rightForce;
+	public bool deathOnImpact = true;
+
+
+	// Originally OnCollisionEnter2D(Collision2D col)
+	void OnTriggerEnter2D(Collider2D col){
+		GameObject foe = col.gameObject;
+		if(foe.GetComponentInChildren<BonHealth>()){
+			BonHealth badHealth = foe.GetComponentInChildren<BonHealth>();
+			badHealth.GainHealth(healing);
+			if (foe.GetComponentInParent<Rigidbody2D>()){
+				Rigidbody2D badrb2d = foe.GetComponentInParent<Rigidbody2D>();
+				print ("contact");
+				badrb2d.AddForce(new Vector2(rightForce, upForce), ForceMode2D.Force);
+				if (deathOnImpact){
+					Destroy(gameObject,0.1f);
+				}
+			}
+		}
+	}
+}
